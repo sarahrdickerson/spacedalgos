0d27c43fa98e31746fb613806a5a29d323efe7a2
sarahrdickerson <sarahrdickerson@gmail.com>
2026-02-24 18:42:40 -0600

    basic navbar updates

--- app/(protected)/dash/page.tsx
+++ app/(protected)/dash/page.tsx
@@ -16,7 +16,7 @@ async function UserDetails() {
   return JSON.stringify(data.claims, null, 2);
 }
 
-export default function ProtectedPage() {
+export default function DashPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
       <div className="w-full">
@@ -28,7 +28,7 @@ export default function ProtectedPage() {
       </div>
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
+        <pre className="text-xs font-mono p-3 rounded border border-muted max-h-32 overflow-auto">
           <Suspense>
             <UserDetails />
           </Suspense>
